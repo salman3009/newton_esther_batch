7a611c02a69bebb36694d77f54809d44351219a2
salman3009 <salman444g@gmail.com>
2023-07-22 12:39:31 +0530

    code

--- Course/July/22-07-23-logger-template-engine/logger/app.js
+++ Course/July/22-07-23-logger-template-engine/logger/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+const logger = require('./logger');
 
 app.get('/',(req,res,next)=>{
+    logger.info('It is entering the api get method');
     res.status(200).send("Successfully fetched the data");
 })
 
 app.get('/info',(req,res,next)=>{
     try{
+        logger.info('It is entering the api get method - info');
        throw new Error("user is not found");
     }catch(err){
+        logger.info('It is entering the api get method -info',err);
      res.status(500).send({message:err.message})
     }
 })
