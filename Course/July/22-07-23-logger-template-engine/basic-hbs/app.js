c2ea0711c48137d5ebbe2b8d702962a98908471b
salman3009 <salman444g@gmail.com>
2023-07-22 13:16:18 +0530

    code

--- Course/July/22-07-23-logger-template-engine/basic-hbs/app.js
+++ Course/July/22-07-23-logger-template-engine/basic-hbs/app.js
@@ -17,6 +17,27 @@ app.get('/dynamic',(req,res)=>{
        res.render('dynamic',{personData:personDetails});
 })
 
+app.get('/projects',(req,res)=>{
+       let projects={
+           fullName:'Rahul',
+           skills:[{
+              type:'Data Mining',
+              rate:3
+           },
+           {
+            type:'Block chain',
+            rate:2
+           },
+           {
+            type:'Nodejs',
+            rate:3
+           }
+        ]
+       };
+       res.render('projects',{projects,projects})
+
+})
+
 app.listen(8080,()=>{
     console.log("server is running on 3000");
 })
