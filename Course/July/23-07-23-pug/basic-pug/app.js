031933236297468223804f94f9d77fe48813a2d2
salman3009 <salman444g@gmail.com>
2023-07-23 10:17:52 +0530

    code

---
+++ Course/July/23-07-23-pug/basic-pug/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('views','./views');
+app.set('view engine','pug');
+
+app.get('/',(req,res)=>{
+     res.render('index');
+})
+
+app.listen(8080,()=>{
+     console.log("server is running on 8080");
+})
